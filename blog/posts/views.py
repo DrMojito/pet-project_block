ec385865bf16efbebb4ddd5e644e8000b2beae02
Ilyha <ilyhaxa20@gmail.com>
2023-09-20 14:45:48 +0300

    add DB and model

--- blog/posts/views.py
+++ blog/posts/views.py
@@ -1,17 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Post, Group
 
 
 def index(request):
     template = 'posts/index.html'
+    posts = Post.objects.order_by('-pub_date')[:10]
     context = {
-        'title': "Главная страница"
+        'title': "Главная страница",
+        'posts': posts,
     }
     return render(request, template, context)
 
 
 def group_posts(request, slug):
     template = 'posts/group_list.html'
+    group = get_object_or_404(Group, slug=slug)
+    posts = Post.objects.filter(group=group).order_by('-pub_date')[:10]
     context = {
-        'title': "Здесь информация о мороженном}"
+        'title': "Здесь информация о мороженном}",
+        'posts': posts,
+        'group': group
     }
     return render(request, template, context)
